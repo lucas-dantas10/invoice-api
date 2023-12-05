73a4974486efa125eebf9ea0daa4b38514aedc74
Lucas <lucas.dantas.nogueira@gmail.com>
2023-12-05 11:00:33 -0300

    :sparkles: feat: criado retorno de uma list dos usuarios e mostrar usuario especifico

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        return UserResource::collection(User::all());
+    }
+
+    public function show(User $user)
+    {
+        return new UserResource($user);
+    }
 }
