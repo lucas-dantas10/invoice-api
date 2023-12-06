b3d6ec0bbc9013eaa218f61db958e3c7b146b16b
Lucas <lucas.dantas.nogueira@gmail.com>
2023-12-06 11:48:38 -0300

    :sparkles: feat: adicionada rota de update e delete de invoices

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,6 @@ use Illuminate\Support\Facades\Route;
 Route::prefix("v1")->group(function () {
     Route::get("users", [UserController::class, "index"])->name("users");
     Route::get("users/{user}", [UserController::class, "show"])->name("users.show");
-    Route::get("invoices", [InvoiceController::class, "index"])->name("invoices");
-    Route::post("invoices", [InvoiceController::class, "store"])->name("invoices.store");
+
+    Route::apiResource('invoices', InvoiceController::class);
 });
