10b40126c68a94470c23d960ff6d6a45d49b8ce0
Lucas <lucas.dantas.nogueira@gmail.com>
2023-12-06 11:50:09 -0300

    :sparkles: feat: adicionada anotações das rotas

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -9,11 +9,35 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    /**
+     * @OA\Info(title="My First API", version="0.1")
+     * @OA\Get(
+     *     tags={"users"},
+     *     summary="Returns a list of users",
+     *     description="Returns a object of users",
+     *     path="/api/v1/users",
+     *     @OA\Response(response="200", description="A list with users"),
+     * ),
+    */
     public function index()
     {
         return UserResource::collection(User::all());
     }
 
+    /**
+     * @OA\Get(
+     *     tags={"users"},
+     *     summary="Returns a user especific",
+     *     description="Returns a object of users",
+     *     path="/api/v1/users/{user_id}",
+     *     @OA\Response(response="200", description="A user especific"),
+     *     @OA\Parameter(
+     *         name="user_id",
+     *         in= "path",
+     *         required=true,
+     *     ),
+     * ),
+    */
     public function show(User $user)
     {
         return new UserResource($user);
