5be3e3234d383a85561c730e17a4835813f7c806
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-12-11 13:03:49 -0300

    :sparkles: feat: criado controlador de autenticação, metodo de login cria um token para um ususuario existente e logado, rota de logout remove o token do usuario

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Traits\HttpResponses;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+// 3|663xhy8hFvJbTPVsZalFQgAxsXRP2bNMOt8jxvud5a60940c -> invoice
+// 4|O6NsHUhTv4eCnv0BvsbULHyOzvZsFLUGCSWkbYCtd3a28eac -> user
+class AuthController extends Controller
+{
+    use HttpResponses;
+
+    public function login(Request $request)
+    {
+        if (Auth::attempt($request->only('email', 'password'))) {
+            return $this->response('Authorized', 200, [
+                'token' => $request->user()->createToken('invoice')->plainTextToken,
+            ]);
+        }
+
+        return $this->error('Not Authorized', 403);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return $this->response('Token Revoked', 200);
+    }
+}
