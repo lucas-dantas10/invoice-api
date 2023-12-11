f76e8f74f3e653199412311978d8de6f10587aef
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-12-11 13:02:51 -0300

    :sparkles: feat: adicionado middleware sanctum para algumas rotas

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\V1\InvoiceController;
 use App\Http\Controllers\Api\V1\UserController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,8 +11,12 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::prefix("v1")->group(function () {
-    Route::get("users", [UserController::class, "index"])->name("users");
-    Route::get("users/{user}", [UserController::class, "show"])->name("users.show");
-
+    Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
     Route::apiResource('invoices', InvoiceController::class);
+    Route::middleware(['auth:sanctum'])->group(function () {
+        Route::get("users", [UserController::class, "index"])->name("users");
+        Route::get("users/{user}", [UserController::class, "show"])->name("users.show");
+        Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+    });
+    
 });
