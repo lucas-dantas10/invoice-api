ffa1ded28c1f1d8f6d80df312a1b44a8f072bc98
Lucas <lucas.dantas.nogueira@gmail.com>
2023-12-14 15:47:57 -0300

    :sparkles: feat: adicionado anotações das configurações de Schema do swagger

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,18 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ *
+ * @OA\Schema(
+ * required={"password"},
+ * @OA\Xml(name="User"),
+ * @OA\Property(property="id", type="integer", readOnly="true", example="1"),
+ * @OA\Property(property="first_name", type="string", maxLength=32, example="John"),
+ * @OA\Property(property="last_name", type="string", maxLength=32, example="Doe"),
+ * @OA\Property(property="email", type="string", readOnly="true", format="email", description="User unique email address", example="user@gmail.com"),
+ * @OA\Property(property="email_verified_at", type="string", readOnly="true", format="date-time", description="Datetime marker of verification status", example="2019-02-25 12:59:20"),
+ * )
+ **/
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -22,6 +34,7 @@ class User extends Authenticatable
         'last_name',
         'email',
         'password',
+        'created_at'
     ];
 
     /**
