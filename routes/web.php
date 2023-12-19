89f1978062ac484f9178585955995f39e1023ffc
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-12-19 15:25:54 -0300

    :sparkles: feat: adicionado redirecionamento para swagger-ui

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return redirect('api/documentation');
 });
 
 require __DIR__.'/auth.php';
